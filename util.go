436211965ec18c1a1ed831ed1c27421902b5567a
jeppeter <jeppeter@gmail.com>
2018-02-23 15:57:50 +0800

    to add logutil.go

--- util.go
+++ util.go
@@ -39,3 +39,13 @@ func formatMap(kattr map[string]string) string {
 	}
 	return s
 }
+
+const (
+	COMMAND_SET              = 10
+	SUB_COMMAND_JSON_SET     = 20
+	COMMAND_JSON_SET         = 30
+	ENVIRONMENT_SET          = 40
+	ENV_SUB_COMMAND_JSON_SET = 50
+	ENV_COMMAND_JSON_SET     = 60
+	DEFAULT_SET              = 70
+)

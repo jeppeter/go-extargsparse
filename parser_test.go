b1ece924e413a62bdcad5e7507a144611544ea32
jeppeter <jeppeter@gmail.com>
2018-03-01 11:03:28 +0800

    pass Test_parser_A019

--- parser_test.go
+++ parser_test.go
@@ -1190,7 +1190,6 @@ func Test_parser_A017(t *testing.T) {
 	check_equal(t, args.GetArray("args"), []string{})
 	return
 }
-*/
 
 func Test_parser_A018(t *testing.T) {
 	var loads = `        {
@@ -1202,7 +1201,7 @@ func Test_parser_A018(t *testing.T) {
             "$port|p" : {
                 "value" : 3000,
                 "type" : "int",
-                "nargs" : 1 , 
+                "nargs" : 1 ,
                 "helpinfo" : "port to connect"
             }
         }`
@@ -1224,5 +1223,78 @@ func Test_parser_A018(t *testing.T) {
 	check_equal(t, args.GetInt("port"), 3000)
 	check_equal(t, args.GetString("dpkg_dpkg"), "dpkg")
 	check_equal(t, args.GetArray("args"), []string{})
+	return
+}
+*/
+
+func Test_parser_A019(t *testing.T) {
+	var loads = `        {
+            "verbose|v" : "+",
+            "$port|p" : {
+                "value" : 3000,
+                "type" : "int",
+                "nargs" : 1 , 
+                "helpinfo" : "port to connect"
+            },
+            "dep" : {
+                "list|l" : [],
+                "string|s" : "s_var",
+                "$" : "+"
+            }
+        }`
+	var err error
+	var parser *ExtArgsParse
+	var params []string
+	var args *NameSpaceEx
+	var jsonfile string = ""
+	var depjsonfile string = ""
+	var ok bool = false
+	var depstrval string
+	var depliststr string
+	beforeParser(t)
+
+	depstrval = "newval"
+	depliststr = `["depenv1","depenv2"]`
+	jsonfile = makeWriteTempFile(`{"dep":{"list" : ["jsonval1","jsonval2"],"string" : "jsonstring"},"port":6000,"verbose":3}`)
+	defer func() {
+		if len(jsonfile) > 0 {
+			if ok {
+				os.Remove(jsonfile)
+			} else {
+				keyDebug("jsonfile [%s]", jsonfile)
+			}
+			jsonfile = ""
+		}
+	}()
+	depjsonfile = makeWriteTempFile(`{"list":["depjson1","depjson2"]}`)
+	defer func() {
+		if len(depjsonfile) > 0 {
+			if ok {
+				os.Remove(depjsonfile)
+			} else {
+				keyDebug("depjsonfile [%s]", depjsonfile)
+			}
+			depjsonfile = ""
+		}
+	}()
+	os.Setenv("EXTARGSPARSE_JSON", jsonfile)
+	os.Setenv("DEP_JSON", depjsonfile)
+
+	parser, err = NewExtArgsParse(nil, []int{ENV_COMMAND_JSON_SET, ENVIRONMENT_SET, ENV_SUB_COMMAND_JSON_SET})
+	check_equal(t, err, nil)
+	err = parser.LoadCommandLineString(fmt.Sprintf("%s", loads))
+	check_equal(t, err, nil)
+	os.Setenv("DEP_STRING", depstrval)
+	os.Setenv("DEP_LIST", depliststr)
+	params = []string{"-p", "9000", "dep", "--dep-string", "ee", "ww"}
+	args, err = parser.ParseCommandLine(params, nil)
+	check_equal(t, err, nil)
+	check_equal(t, args.GetInt("verbose"), 3)
+	check_equal(t, args.GetInt("port"), 9000)
+	check_equal(t, args.GetString("subcommand"), "dep")
+	check_equal(t, args.GetArray("dep_list"), []string{"jsonval1", "jsonval2"})
+	check_equal(t, args.GetString("dep_string"), "ee")
+	check_equal(t, args.GetArray("subnargs"), []string{"ww"})
+	ok = true
 	return
 }
